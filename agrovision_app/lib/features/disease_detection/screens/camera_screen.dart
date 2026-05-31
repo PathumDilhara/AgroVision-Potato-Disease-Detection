e4b1f69d3ec985d270ca5bd39b7d80c848087249
Shashika Pathum Dilhara <161570319+PathumDilhara@users.noreply.github.com>
2026-05-31 18:37:28 +0530

    Image captureing with camera implementing

---
+++ agrovision_app/lib/features/disease_detection/screens/camera_screen.dart
@@ -0,0 +1,143 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  final CameraDescription camera;
+  const CameraScreen({super.key, required this.camera});
+
+  @override
+  State<CameraScreen> createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(widget.camera, ResolutionPreset.medium);
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _takePicture() async {
+    try {
+      await _initializeControllerFuture;
+      final image = await _controller.takePicture();
+
+      if (!mounted) return;
+
+      print("#### ${image.path}");
+
+      _buildDialogBox(image.path);
+
+      // Navigator.pop(context, image.path);
+    } catch (err) {
+      debugPrint("Error taking picture: $err");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text(
+                "Error",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Stack(
+              children: [
+                CameraPreview(_controller),
+
+                // buttons
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: FloatingActionButton(
+                      backgroundColor: Colors.green,
+                      onPressed: () {
+                        _takePicture();
+                      },
+                      child: Icon(Icons.camera, color: Colors.black),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(color: Colors.green),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  void _buildDialogBox(String path) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.file(File(path), fit: BoxFit.cover),
+              SizedBox(height: 10),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.greenAccent),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      "Try again",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.orange),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.pop(context, path);
+                    },
+                    child: Text("Done", style: TextStyle(color: Colors.black)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
--- agrovision_app/lib/features/disease_detection/screens/home_screen.dart
+++ agrovision_app/lib/features/disease_detection/screens/home_screen.dart
@@ -1,7 +1,18 @@
+import 'dart:io';
+
+import 'package:agrovision_app/features/disease_detection/screens/camera_screen.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final ValueNotifier<String> _imgPath = ValueNotifier("");
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +26,159 @@ class HomeScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Text(
-          "Home screen",
-          style: TextStyle(
-            fontSize: 26,
-            fontWeight: FontWeight.bold,
-            color: Colors.black45,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(height: 30),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.red.withValues(alpha: 0.5),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "Capture or upload a potato leaf image and let the "
+                      "AI model detect potential diseases with confidence "
+                      "scores.",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll(
+                              Colors.orange,
+                            ),
+                          ),
+                          onPressed: () async {
+                            print("Capture");
+
+                            _imgPath.value = "";
+
+                            final cameras = await availableCameras();
+                            String result = "";
+
+                            if (context.mounted) {
+                              result = await Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder:
+                                      (_) => CameraScreen(camera: cameras[0]),
+                                ),
+                              );
+                            }
+
+                            if (result.isNotEmpty) {
+                              // result is image path
+                              print("### Captured image: $result");
+                              _imgPath.value = result;
+                            }
+                          },
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.camera_alt,
+                                size: 20,
+                                color: Colors.black,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "Capture",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll(
+                              Colors.blue,
+                            ),
+                          ),
+                          onPressed: () {
+                            print("import");
+                          },
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.add_circle,
+                                size: 20,
+                                color: Colors.black,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "Import",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+
+              ValueListenableBuilder(
+                valueListenable: _imgPath,
+                builder: (context, value, child) {
+                  return Container(
+                    padding: EdgeInsets.all(16),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.blue.withValues(alpha: 0.3),
+                    ),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          height: MediaQuery.of(context).size.width * 0.8,
+                          child:
+                              value.isEmpty
+                                  ? Icon(
+                                    Icons.image_not_supported_outlined,
+                                    size: 200,
+                                    color: Colors.grey,
+                                  )
+                                  : Image.file(
+                                    File(value),
+                                    fit: BoxFit.fitWidth,
+                                  ),
+                        ),
+
+                        SizedBox(height: 20),
+
+                        Text(
+                          "Early__Blight\nConfidence : 100%",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
